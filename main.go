6d5fff05cd60be35a855b468e824ac94083256ea
mskaife <mskaife@jlr1ukxl1delvdm.lwap.jlrint.com>
2020-01-20 17:22:37 +0000

    Add 404 not found response on home handler

--- main.go
+++ main.go
@@ -8,14 +8,33 @@ import (
 // Define home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/".
+	// If it doesn't then use the http.NotFound() function to return a 404 response.
+	// We then return from the handler to avoid continuing execution
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Handler to show a specific snippet
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+// Handler to create a new snippet
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
 }
 
 func main() {
 	// Use the new http.NewServeMux() function to initialise a new servemux,
-	// then register the home() function as the handler for the "/" URL pattern
+	// then register handler functions for each URL pattern
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// Use the http.ListenAndServe() function to start a new web server.
 	// We pass in two parameters:
