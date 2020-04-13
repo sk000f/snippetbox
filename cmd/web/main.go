3db6c1aaf85bacccd1825ca7572202549c6b4f37
sk000f <mike@skaife.me>
2020-04-13 14:12:53 +0100

    refactoring to move neutered file system into its own file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -39,26 +38,4 @@ type application struct {
 
 type config struct {
 	addr string
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if s.IsDir() {
-		index := strings.TrimSuffix(path, "/") + "/index.html"
-		if _, err := nfs.fs.Open(index); err != nil {
-			return nil, err
-		}
-	}
-
-	return f, nil
 }
