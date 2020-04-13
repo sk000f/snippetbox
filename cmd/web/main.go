2f439fd7acb012c8590f3c476b32206128e7cc58
sk000f <mike@skaife.me>
2020-04-13 14:57:24 +0100

    adding initial MySQL database setup and connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	cfg := new(config)
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.dsn, "dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -27,7 +37,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", cfg.addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
 
@@ -38,4 +48,16 @@ type application struct {
 
 type config struct {
 	addr string
+	dsn  string
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sk000f/snippetbox
 
 go 1.13
 
-require github.com/coreos/etcd v3.3.20+incompatible
+require (
+	github.com/coreos/etcd v3.3.20+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
+)
