a935ecea1d29cdbf1b1cfaa4a5ef55542da1ba0e
sk000f <mike@skaife.me>
2020-04-13 10:58:18 +0100

    implement custom FileSystem to prevent directory listing for static folder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -12,10 +13,33 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// Serve static files from the ./ui/static/ directory
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := nfs.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
