ddb110c51a44264210c3582ca04c7a8bbaf87614
sk000f <mike@skaife.me>
2020-04-13 15:54:38 +0100

    small refactoring, run query and scan in same statement, errors deferred until scan called

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -35,11 +35,9 @@ func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
-	row := m.DB.QueryRow(stmt, id)
-
 	s := &models.Snippet{}
 
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	err := m.DB.QueryRow(stmt, id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, models.ErrNoRecord
