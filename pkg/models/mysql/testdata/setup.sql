6bd70a8b5a9f1ee16cd8491b76f64f950c03d143
sk000f <mike@skaife.me>
2020-04-15 14:10:39 +0100

    add integration test for interacting with users in database

--- pkg/models/mysql/testdata/setup.sql
+++ pkg/models/mysql/testdata/setup.sql
@@ -20,8 +20,8 @@ CREATE TABLE users (
 ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
 
 INSERT INTO users (name, email, hashed_password, created) VALUES (
-  'Alice Jones',
-  'alice@example.com',
-  '$2a$12$NuTjWXm3KKntReFwyBVHyuf/to.HEwTy.eS206TNfkGfr6HzGJSWG',
-  '2018-12-23 17:25:22'
+    'Alice Jones',
+    'alice@example.com',
+    '$2a$12$NuTjWXm3KKntReFwyBVHyuf/to.HEwTy.eS206TNfkGfr6HzGJSWG',
+    '2018-12-23 17:25:22'
 );
---
+++ pkg/models/mysql/testutils_test.go
@@ -0,0 +1,40 @@
+package mysql
+
+import (
+	"database/sql"
+	"io/ioutil"
+	"testing"
+)
+
+func newTestDB(t *testing.T) (*sql.DB, func()) {
+	// Establish DB connection and set multi statement flag to run all of setup script
+	db, err := sql.Open("mysql", "test_web:pass@/test_snippetbox?parseTime=true&multiStatements=true")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	script, err := ioutil.ReadFile("./testdata/setup.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = db.Exec(string(script))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Return connection pool and anonymous function to close it and teardown data
+	return db, func() {
+		script, err := ioutil.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		db.Close()
+	}
+}
